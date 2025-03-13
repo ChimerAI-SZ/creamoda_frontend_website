79b974ade8388cdc3d6ae9597d1c3de06533c419
柴卷 <oukyuushiba@gmail.com>
2025-03-13 19:21:03 +0800

    fix: header 样式调整

--- components/Header/components/Logo.tsx
+++ components/Header/components/Logo.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function Logo() {
   return (
-    <div className="flex items-center">
-      <span className="ml-2 text-xl font-bold">CREAMODA</span>
+    <div className="flex items-center h-[20px] w-[155px]">
+      <Image src="/images/logo-black.svg" alt="Logo" width={155} height={20} />
     </div>
   );
 }
