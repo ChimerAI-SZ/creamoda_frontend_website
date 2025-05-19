f1335105c5770182639f1a7c127679654ca0b964
柴卷 <oukyuushiba@gmail.com>
2025-05-19 15:10:22 +0800

    fix: 编译报错修正

--- app/virtual-try-on/page.tsx
+++ app/virtual-try-on/page.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from 'react';
+
 import { ImageGrid } from '@/components/ImageGrid';
 import { Sidebar } from './components/Sidebar';
 
 export default function Page() {
   return (
     <div className="flex">
-      <Sidebar />
+      <Suspense fallback={<div></div>}>
+        <Sidebar />
+      </Suspense>
       <main className="flex-1 z-0 p-[16px] h-[calc(100vh-64px)] overflow-y-auto bg-[#FFFDFA]">
         <ImageGrid />
       </main>
