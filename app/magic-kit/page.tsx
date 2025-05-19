1d3c961c22fd044f94ae7f374e4f54f544169d3b
柴卷 <oukyuushiba@gmail.com>
2025-05-19 17:26:44 +0800

    fix: album 入口恢复

--- app/magic-kit/page.tsx
+++ app/magic-kit/page.tsx
@@ -1,14 +1,19 @@
 'use client';
+
 import { ImageGrid } from '@/components/ImageGrid';
 import { Sidebar } from '@/app/magic-kit/components/Sidebar';
+import { Album as AlbumDrawer } from '@/components/Album';
 
 export default function Page() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="flex-1 z-0 p-[16px] h-[calc(100vh-64px)] overflow-y-auto bg-[#FFFDFA]">
-        <ImageGrid />
-      </main>
-    </div>
+    <>
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 z-0 p-[16px] h-[calc(100vh-64px)] overflow-y-auto bg-[#FFFDFA]">
+          <ImageGrid />
+        </main>
+        <AlbumDrawer />
+      </div>
+    </>
   );
 }
--- app/virtual-try-on/page.tsx
+++ app/virtual-try-on/page.tsx
@@ -2,16 +2,20 @@ import { Suspense } from 'react';
 
 import { ImageGrid } from '@/components/ImageGrid';
 import { Sidebar } from './components/Sidebar';
+import { Album as AlbumDrawer } from '@/components/Album';
 
 export default function Page() {
   return (
-    <div className="flex">
-      <Suspense fallback={<div></div>}>
-        <Sidebar />
-      </Suspense>
-      <main className="flex-1 z-0 p-[16px] h-[calc(100vh-64px)] overflow-y-auto bg-[#FFFDFA]">
-        <ImageGrid />
-      </main>
-    </div>
+    <>
+      <div className="flex">
+        <Suspense fallback={<div></div>}>
+          <Sidebar />
+        </Suspense>
+        <main className="flex-1 z-0 p-[16px] h-[calc(100vh-64px)] overflow-y-auto bg-[#FFFDFA]">
+          <ImageGrid />
+        </main>
+      </div>
+      <AlbumDrawer />
+    </>
   );
 }
