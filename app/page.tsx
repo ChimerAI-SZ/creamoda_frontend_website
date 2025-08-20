20483bdedbd83ee5d3f45f0b66550be29e9a4b1d
李丹欣 <1784325610@qq.com>
2025-08-20 15:03:13 +0800

    修复：将主页 Sidebar 组件也包含在 Suspense 边界内

--- app/page.tsx
+++ app/page.tsx
@@ -34,8 +34,8 @@ export default function Page() {
     <div className="flex p-6 pt-[30px] z-0">
       <Suspense fallback={<div>Loading...</div>}>
         <SearchParamsHandler />
+        <Sidebar />
       </Suspense>
-      <Sidebar />
       <main className="flex-1 pl-6 h-[calc(100vh-110px)] overflow-y-auto bg-transparent">
         <ImageGrid />
       </main>
